a6441e03cee7770f049f791737de616368d06a3d
josertgg <josetara4666@gmail.com>
2024-05-07 10:14:16 -0400

    End First Exercises

---
+++ one.py
@@ -0,0 +1,14 @@
+import random
+
+def person(x):
+    random_number = random.randint(1, x)
+    person = 0
+    while person != random_number:
+        person = int(input(f'Guess a number between 1 and {x}: '))
+        if person < random_number:
+            print('Sorry, guess again. Too low.')
+        elif person > random_number:
+            print('Sorry, guess again. Too high.')
+
+    print(f'Yay, congrats. You have guessed the number {random_number} correctly!!')
+
